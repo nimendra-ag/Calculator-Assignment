d69b863ef02817cd91034f22a44842707c9f950d
Nimendra Gunawardana <nimendraamiththa1@gmail.com>
2023-06-03 23:47:31 +0530

    This is the first stage of the project

---
+++ index.js
@@ -0,0 +1,11 @@
+function number1(){
+    var first_operand = 1;
+    document.getElementById("inputTab").value = 1; 
+    
+}
+
+var isCalculatorOn = true;
+while (isCalculatorOn){
+    let first_num = 8;
+    let 
+}
