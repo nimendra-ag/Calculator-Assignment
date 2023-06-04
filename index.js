f5916d7c3cc13a1a828e178a1418601966d0318e
Nimendra Gunawardana <nimendraamiththa1@gmail.com>
2023-06-04 15:05:16 +0530

    add the bodmas rule proparly

--- index.js
+++ index.js
@@ -29,6 +29,25 @@ class Calculator {
     return this;
   }
 
+  
+
+  applyBodmas(){
+    for(let i = 0; i<this.operationQueue.length; i++){
+        console.log("round", i);
+      if(this.operationQueue[i] == "*"|| this.operationQueue[i] == "/"){
+        var numberToBeReplaced = this.performOperation(this.operationQueue[i-1], this.operationQueue[i], this.operationQueue[i+1])
+        console.log(this.operationQueue);
+          this.operationQueue[i] = numberToBeReplaced;
+        console.log(this.operationQueue);
+        this.operationQueue.splice(i+1, 1);
+          console.log(this.operationQueue);
+        this.operationQueue.splice(i-1, 1);
+          console.log(this.operationQueue);
+      }
+    }
+      console.log(this.operationQueue);
+      return this;
+  }
   calculate() {
     let currentResult = 0;
     let currentOperator = null;
@@ -81,10 +100,11 @@ class Calculator {
 
 let calculator = new Calculator();
 
-let result = calculator.number(5).multiply(3).subtract(10).divide(2).calculate().getResult();
+let result = calculator.number(5).multiply().number(10).divide().number(5).applyBodmas().calculate().getResult();
 console.log(result); // Output: -1.5
 
+
 calculator.clear();
-result = calculator.number(4).add().number(2).multiply(3).calculate().getResult();
+result = calculator.number(4).applyBodmas().calculate().getResult();
 console.log(result); // Output: 18
 
