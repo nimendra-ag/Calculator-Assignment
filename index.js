472d4a5ff11eee67e58d465a8c15b57ded812a91
Nimendra Gunawardana <nimendraamiththa1@gmail.com>
2023-06-05 18:04:51 +0530

    any calculation can be operated according to the bodmas rule

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ class Calculator {
     this.result = 0;
     this.operationQueue = [];
     this.entryText = "";
+    this.IdArray = ["button_1", "button_2", "button_3", "button_=", "button_-"]
   }
-
   updateEntry(){
     for(let i = 0; i<this.operationQueue.length; i++){
       this.entryText += this.operationQueue[i];
@@ -64,6 +64,8 @@ class Calculator {
       return this;
   }
   calculate() {
+    
+    this.applyBodmas();
     let currentResult = 0;
     let currentOperator = null;
 
@@ -84,7 +86,10 @@ class Calculator {
 
     this.result = currentResult;
     this.operationQueue = [];
-    return this;
+    this.entryText = this.result;
+    console.log(this.result);
+    document.getElementById("entry").value = this.entryText;
+    // return this;
   }
 
   performOperation(num1, operator, num2) {
@@ -115,12 +120,97 @@ class Calculator {
 
 let calculator = new Calculator();
 
-var number_1 = document.getElementById("number_1");
+var button_1 = document.getElementById("button_1");
+var button_2 = document.getElementById("button_2");
+var button_3 = document.getElementById("button_3");
+var button_4 = document.getElementById("button_4");
+var button_5 = document.getElementById("button_5");
+var button_6 = document.getElementById("button_6");
+var button_7 = document.getElementById("button_7");
+var button_8 = document.getElementById("button_8");
+var button_9 = document.getElementById("button_9");
+var button_0 = document.getElementById("button_0");
+var button_00 = document.getElementById("button_00");
+var button_dot = document.getElementById("button_1");
+var button_plus = document.getElementById("button_+");
+var button_sqrt = document.getElementById("button_1");
+var button_equal = document.getElementById("button_=");
+var button_minus = document.getElementById("button_-");
+var button_oneover = document.getElementById("button_1");
+var button_multiplication = document.getElementById("button_*");
+var button_division = document.getElementById("button_/");
+var button_percentage = document.getElementById("button_1");
+var button_check = document.getElementById("button_1");
+var button_plusorminus = document.getElementById("button_1");
+var button_Mminus = document.getElementById("button_1");
+var button_Mplus = document.getElementById("button_1");
+var button_Mc = document.getElementById("button_1");
+var button_Mr= document.getElementById("button_1");
+var button_Ms = document.getElementById("button_1");
+var button_leftarrow = document.getElementById("button_1");
+var button_ce = document.getElementById("button_1");
+var button_c = document.getElementById("button_1");
 
-number_1.addEventListener("click", () => {
+
+button_1.addEventListener("click", () => {
   calculator.number(1);
-  // document.getElementById("entry").value = 1;
 })
+
+button_2.addEventListener("click", ()=> {
+  calculator.number(2);
+})
+
+button_3.addEventListener("click", ()=> {
+  calculator.number(3);
+})
+
+button_4.addEventListener("click", ()=> {
+  calculator.number(4);
+})
+
+button_5.addEventListener("click", ()=> {
+  calculator.number(5);
+})
+
+button_6.addEventListener("click", ()=> {
+  calculator.number(6);
+})
+
+button_7.addEventListener("click", ()=> {
+  calculator.number(7);
+})
+
+button_8.addEventListener("click", ()=> {
+  calculator.number(8);
+})
+
+button_9.addEventListener("click", ()=>{
+  calculator.number(9)
+})
+
+button_plus.addEventListener("click", ()=>{
+  calculator.add()
+})
+
+button_minus.addEventListener("click", ()=>{
+  calculator.subtract()
+})
+
+button_division.addEventListener("click", ()=>{
+  calculator.divide()
+})
+
+button_multiplication.addEventListener("click", ()=>{
+  calculator.multiply()
+})
+
+button_equal.addEventListener("click", ()=>{
+  calculator.calculate();
+})
+
+
+
+
 
 // let result = calculator.number(5).multiply().number(10).divide().number(5).applyBodmas().calculate().getResult();
 // console.log(result); // Output: -1.5
